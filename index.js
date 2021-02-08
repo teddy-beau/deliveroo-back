393ac897c821389f8c23affe5de35d5f1ba9dfd3
teddy-beau <hello@teddy-beau.com>
2021-02-08 13:59:01 +0100

    project set up

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
+
+app.get("/", (req, res) => {});
+
+app.listen(process.env.PORT || 3000, () => {
+   console.log("Server started");
+});
